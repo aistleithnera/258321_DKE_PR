ead8b7ccc5b9836a49627fa9ff168133beb5b4d3
ateuchtmann <34478804+ateuchtmann@users.noreply.github.com>
2019-01-15 18:54:10 +0100

    Implementierung
    Vadalog

--- Implementierung/Temp/EvaluationFramework/src/Vadalog/VadalogExecution.java
+++ Implementierung/Temp/EvaluationFramework/src/Vadalog/VadalogExecution.java
@@ -2,38 +2,30 @@ package Vadalog;
 
 import java.util.concurrent.ThreadLocalRandom;
 
-import DBConnection.DBSaveEntry;
-import Models.CBR;
-import Models.RMI;
-
 public class VadalogExecution {
 
-	public static void calcCBR(CBR cbr) {
+	public static double calcExTime() {
 		
 		double randExTime = ThreadLocalRandom.current().nextDouble(1, 500);
-		boolean randNoErrors = true;
-		double randCpuUsage = ThreadLocalRandom.current().nextDouble(1, 101);
 		
-		cbr.setExTime(randExTime);
-		cbr.setErrors(randNoErrors);
-		cbr.setExTime(randCpuUsage);
-		
-		DBSaveEntry.newCBR(cbr);
+		return randExTime;
 		
 	}
 	
-	public static void calcRMI(RMI rmi) {
+	public static boolean calcNoErrors() {
 		
-		double randExTime = ThreadLocalRandom.current().nextDouble(1, 500);
 		boolean randNoErrors = false;
+
+		return randNoErrors;
+		
+	}
+	
+	public static double calcCpuUsage() {
+		
 		double randCpuUsage = ThreadLocalRandom.current().nextDouble(1, 101);
 		
-		rmi.setExTime(randExTime);
-		rmi.setErrors(randNoErrors);
-		rmi.setExTime(randCpuUsage);
+		return randCpuUsage;
 		
-		DBSaveEntry.newRMI(rmi);
-		
-	}	
+	}
 	
 }
